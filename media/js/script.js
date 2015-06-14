69fe6cc77c78990867f8bdde867dac4bbcf9f0d5
jakimfett <jakimfett@picraft.com>
2015-06-14 01:41:41 -0700

    Adjust for new nav position

--- media/js/script.js
+++ media/js/script.js
@@ -2,7 +2,7 @@ jQuery("document").ready(function ($) {
     var nav = $('#navigation-container');
     $(window).scroll(function () {
         //console.log($(this).scrollTop());
-        if ($(this).scrollTop() > 485) {
+        if ($(this).scrollTop() > 369) {
             nav.addClass("floating-nav");
         } else {
             nav.removeClass("floating-nav");
