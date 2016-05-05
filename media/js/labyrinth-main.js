3122d36e8bb946208761e789b91aa5e2916ba20d
jakimfett <jakimfett@gmail.com>
2016-05-05 00:49:16 -0700

    Fix formatting

--- media/js/labyrinth-main.js
+++ media/js/labyrinth-main.js
@@ -1,104 +1,112 @@
-(function($) {
-	
-	function expand(el) {
-		var parent = el.parent();
-		var top = el.closest("section");
-		var height = top.height();
-		if (height == 0 || height == NaN)
-			return;
-		
-		var container = top.find(".hsContent");
-		var padout = container.css("padding").split(" ");
-		var pads = [ parseInt(padout[0], 10), parseInt(padout[1], 10) ];
-		
-		var pref_height = height - pads[1];
-		var pref_ratio = parseFloat(el.data("resize-ratio"));
-		var pref_width = pref_ratio * pref_height;
-		
-		var parent_width = parent.width();
-		if (pref_width > parent_width) {
-			pref_width = parent_width;
-			pref_height = pref_width / pref_ratio;
-		}
-		
-		el.width ( pref_width );
-		el.height ( pref_height );
-		return [ pref_width, pref_height ];
-	}
-	
-	function labyrinth() {
-		var $window = $(window);
-		var $fullwidths = $('.macro-fullwidth');
-		var $halfwidths = $('.macro-halfwidth');
-		var $blackouts = $('.blackout');
-		var $medias = $(".media-embed");
-		var $footer = $('.footer');
-		var $body = $('body');
-		
-		function adjustWindow(){
-			// Init Skrollr
-			var s = skrollr.init({
-				forceHeight: false,
-				render: function(data) {
-					//Debugging - Log the current scroll position.
-					//console.log(data.curTop);
-				}
-			});
+(function ($) {
 
-			// Get window size
-			var winH = $window.height();
-			var hWinH = winH / 2;
-			// Keep minimum height 550
-			if (winH <= 600)
-				winH = 550;
-			if (hWinH <= 600)
-				hWinH = 600;
+    function expand(el) {
+        var parent = el.parent();
+        var top = el.closest("section");
+        var height = top.height();
+        if (height == 0 || height == NaN)
+            return;
 
-			// Resize our slides
-			var reperf = function(el, umPrefSz) {
-				el.css("height", "auto");
-				var elAcSz = el.height();
-				if (elAcSz > umPrefSz) 
-					el.css("height", 50 + elAcSz);
-				else
-					el.css("height", umPrefSz); 
-			};
-			
-			reperf($footer, 6 * (winH / 8));
-			$fullwidths.each(function() { 
-				reperf(jQuery(this), winH);
-			});
-			$halfwidths.each(function() { 
-				reperf(jQuery(this), hWinH); 
-			} );
-			$blackouts.each(function() { 
-				reperf(jQuery(this), winH / 8); 
-			});
+        var container = top.find(".hsContent");
+        var padout = container.css("padding").split(" ");
+        var pads = [parseInt(padout[0], 10), parseInt(padout[1], 10)];
 
-			$(".dyn-size").each(function() {
-				expand($(this));
-			});
-			
-			// Refresh Skrollr after resizing our sections
-			s.refresh($('.intro'));
-		}
-		
-		var flip = function() {
-			var $actions = $(".actions");
-			$actions.each(function() { 
-				if ($(this).css("display") == "block")
-					$(this).css("display", "none");
-				else
-					$(this).css("display", "block");
-			});
-			return false;
-		};
-		$("#xpand").on("click", flip);
-	
-		setTimeout(function() { adjustWindow(); $medias.fitVids(); }, 250);
-		$(window).on("resize", function() { adjustWindow(); $medias.fitVids(); });
-		
-	}
-	
-	$(document).on("ready", function() { labyrinth(); });
+        var pref_height = height - pads[1];
+        var pref_ratio = parseFloat(el.data("resize-ratio"));
+        var pref_width = pref_ratio * pref_height;
+
+        var parent_width = parent.width();
+        if (pref_width > parent_width) {
+            pref_width = parent_width;
+            pref_height = pref_width / pref_ratio;
+        }
+
+        el.width(pref_width);
+        el.height(pref_height);
+        return [pref_width, pref_height];
+    }
+
+    function labyrinth() {
+        var $window = $(window);
+        var $fullwidths = $('.macro-fullwidth');
+        var $halfwidths = $('.macro-halfwidth');
+        var $blackouts = $('.blackout');
+        var $medias = $(".media-embed");
+        var $footer = $('.footer');
+        var $body = $('body');
+
+        function adjustWindow() {
+            // Init Skrollr
+            var s = skrollr.init({
+                forceHeight: false,
+                render: function (data) {
+                    //Debugging - Log the current scroll position.
+                    //console.log(data.curTop);
+                }
+            });
+
+            // Get window size
+            var winH = $window.height();
+            var hWinH = winH / 2;
+            // Keep minimum height 550
+            if (winH <= 600)
+                winH = 550;
+            if (hWinH <= 600)
+                hWinH = 600;
+
+            // Resize our slides
+            var reperf = function (el, umPrefSz) {
+                el.css("height", "auto");
+                var elAcSz = el.height();
+                if (elAcSz > umPrefSz)
+                    el.css("height", 50 + elAcSz);
+                else
+                    el.css("height", umPrefSz);
+            };
+
+            reperf($footer, 6 * (winH / 8));
+            $fullwidths.each(function () {
+                reperf(jQuery(this), winH);
+            });
+            $halfwidths.each(function () {
+                reperf(jQuery(this), hWinH);
+            });
+            $blackouts.each(function () {
+                reperf(jQuery(this), winH / 8);
+            });
+
+            $(".dyn-size").each(function () {
+                expand($(this));
+            });
+
+            // Refresh Skrollr after resizing our sections
+            s.refresh($('.intro'));
+        }
+
+        var flip = function () {
+            var $actions = $(".actions");
+            $actions.each(function () {
+                if ($(this).css("display") == "block")
+                    $(this).css("display", "none");
+                else
+                    $(this).css("display", "block");
+            });
+            return false;
+        };
+        $("#xpand").on("click", flip);
+
+        setTimeout(function () {
+            adjustWindow();
+            $medias.fitVids();
+        }, 250);
+        $(window).on("resize", function () {
+            adjustWindow();
+            $medias.fitVids();
+        });
+
+    }
+
+    $(document).on("ready", function () {
+        labyrinth();
+    });
 })(jQuery);
